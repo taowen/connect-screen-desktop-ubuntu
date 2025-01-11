075eae5f4c02593b049e0067fedc80ad9dfbacee
Tao Wen <taowen@gmail.com>
2025-01-11 14:09:49 +0800

    add ubuntu desktop activity

---
+++ app/src/main/java/com/termux/app/UbuntuDesktopActivity.java
@@ -0,0 +1,30 @@
+package com.termux.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.termux.R;
+
+public class UbuntuDesktopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubuntu_desktop);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+} 
